5f4e3bf2819099feba64c7337a6f5bc68381a77b
Sondre Bjellås <pixelworld@hotmail.com>
2019-04-07 14:37:55 +0200

    Add market support to the Insight API
    - Add a proxy against the p2pb2b market API with a cache of 30 seconds.

--- src/City.Chain.Insight/Controllers/MarketsController.cs
+++ src/City.Chain.Insight/Controllers/MarketsController.cs
@@ -1,46 +1,38 @@
-﻿//using System;
-//using System.Collections.Generic;
-//using System.Linq;
-//using System.Threading.Tasks;
-//using Microsoft.AspNetCore.Mvc;
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using RestSharp;
 
-//// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+namespace City.Chain.Insight.Controllers
+{
+	[Route("api/market")]
+	public class MarketsController : Controller
+	{
+		[HttpGet("{id}")]
+		[ResponseCache(Duration = 30)]
+		public string Get(string id)
+		{
+			string market;
 
-//namespace City.Chain.Insight.Controllers
-//{
-//    [Route("api/[controller]")]
-//    public class MarketsController : Controller
-//    {
-//        // GET: api/<controller>
-//        [HttpGet]
-//        public IEnumerable<string> Get()
-//        {
-//            return new string[] { "value1", "value2" };
-//        }
+			if (id.ToLower() == "usd")
+			{
+				market = "CITY_USD";
+			}
+			else
+			{
+				market = "CITY_BTC";
+			}
 
-//        // GET api/<controller>/5
-//        [HttpGet("{id}")]
-//        public string Get(int id)
-//        {
-//            return "value";
-//        }
+			var client = new RestClient("https://p2pb2b.io/");
+			var request = new RestRequest("api/v1/public/ticker?market={market}", Method.GET);
+			request.RequestFormat = DataFormat.Json;
+			request.AddParameter("market", market, ParameterType.QueryString);
 
-//        // POST api/<controller>
-//        [HttpPost]
-//        public void Post([FromBody]string value)
-//        {
-//        }
-
-//        // PUT api/<controller>/5
-//        [HttpPut("{id}")]
-//        public void Put(int id, [FromBody]string value)
-//        {
-//        }
-
-//        // DELETE api/<controller>/5
-//        [HttpDelete("{id}")]
-//        public void Delete(int id)
-//        {
-//        }
-//    }
-//}
+			IRestResponse response = client.Execute(request);
+			var content = response.Content;
+			return content;
+		}
+	}
+}
